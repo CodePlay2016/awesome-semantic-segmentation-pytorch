19efa6823a6e830a21fa4d2a18bdb9aaaf317981
CodePlay2016 <13269308268@163.com>
2019-09-16 17:42:02 +0800

    debug

--- scripts/demo.py
+++ scripts/demo.py
@@ -33,6 +33,7 @@ REP = args.repetition
 
 def demo(config):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print('using device %s' % device)
     # output folder
     if not os.path.exists(config.out_dir):
         os.makedirs(config.out_dir)
