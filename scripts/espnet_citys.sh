56e74e399a6237fbfdb46a0d92d2d51057b65f8a
CodePlay2016 <13269308268@163.com>
2019-09-16 02:09:30 +0000

    train0912

--- scripts/espnet_citys.sh
+++ scripts/espnet_citys.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=4 nohup python train.py --model espnet --backbone resnet101 --dataset citys --lr 0.0001 --epochs 50 --workers=0 > output2.log
+CUDA_VISIBLE_DEVICES=3 nohup python train.py --model espnet --backbone resnet101 --dataset citys --lr 0.0001 --epochs 500 --workers=0 > output4.log
--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=3 nohup python train.py --model deeplabv3 --backbone resnet101 --dataset citys --lr 0.0001 --epochs 50 --workers 0 --save-dir ../model > output.log
+CUDA_VISIBLE_DEVICES=5 nohup python train.py --model deeplabv3 --backbone resnet152 --dataset citys --lr 0.0001 --epochs 200 --workers 0 --save-dir ../model > output3.log
