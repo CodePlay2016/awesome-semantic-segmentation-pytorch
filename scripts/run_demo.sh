ea32376a7e4aa032036c47d5d1c89f5e67a96d75
CodePlay2016 <13269308268@163.com>
2019-09-16 15:27:05 +0800

    config

--- scripts/run_demo.sh
+++ scripts/run_demo.sh
@@ -1,1 +1,2 @@
-python demo.py --model deeplabv3_resnet101_citys --dataset citys --input-pic /home/hufq/data/leftImg8bit_trainvaltest/leftImg8bit/test/berlin/berlin_000192_000019_leftImg8bit.png --out-dir /home/hufq/eval
+# python demo.py --model deeplabv3_resnet101_citys --dataset citys --input-pic /home/hufq/data/leftImg8bit_trainvaltest/leftImg8bit/test/berlin/berlin_000192_000019_leftImg8bit.png --out-dir /home/hufq/eval
+python demo.py --model deeplabv3_resnet101_citys --dataset citys --input-pic /home/hufq/data/apollo_selected/171206_054521441_Camera_5.jpg --out-dir /home/hufq/eval
