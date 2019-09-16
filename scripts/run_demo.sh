3e6d5d28918279e9c5510ada9a1e6b3442468571
CodePlay2016 <13269308268@163.com>
2019-09-16 07:22:04 +0000

    add demo script

---
+++ scripts/run_demo.sh
@@ -0,0 +1,1 @@
+python demo.py --model deeplabv3_resnet101_citys --input-pic /home/hufq/data/leftImg8bit_trainvaltest/leftImg8bit/test/berlin/berlin_000192_000019_leftImg8bit.png --out-dir /home/hufq/eval
