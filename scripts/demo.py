cdc15f3fb222adb5caf31a844eee45bc0cd2597e
CodePlay2016 <13269308268@163.com>
2019-09-17 10:59:02 +0800

    resize image

--- scripts/demo.py
+++ scripts/demo.py
@@ -48,6 +48,7 @@ def demo(config):
     transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        transforms.Resize((480,520))
     ])
     image = Image.open(config.input_pic).convert('RGB')
     images = transform(image).unsqueeze(0).to(device)
