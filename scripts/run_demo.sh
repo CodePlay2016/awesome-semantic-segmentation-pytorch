bf5b1c21ad5d086d7450ad15326f8fd7e5685151
CodePlay2016 <13269308268@163.com>
2019-09-17 11:07:24 +0800

    confgi

--- scripts/run_demo.sh
+++ scripts/run_demo.sh
@@ -1,5 +1,5 @@
 # python demo.py --model deeplabv3_resnet101_citys --dataset citys --input-pic /home/hufq/data/leftImg8bit_trainvaltest/leftImg8bit/test/berlin/berlin_000192_000019_leftImg8bit.png --out-dir /home/hufq/eval
-CUDA_VISIBLE_DEVICE=2 python demo.py --model deeplabv3_resnet101_citys --dataset citys --input-pic /home/hufq/data/apollo_selected/171206_054521441_Camera_5.jpg --out-dir /home/hufq/eval --repetition 10
+CUDA_VISIBLE_DEVICE=2 python demo.py --model deeplabv3_resnet101_citys --dataset citys --input-pic /home/hufq/data/apollo_selected/171206_054521441_Camera_5.jpg --out-dir /home/hufq/eval --repetition 100
 # for img in '/home/hufq/data/apollo_selected'/*5.jpg
 # do
 #     python demo.py --model deeplabv3_resnet101_citys --dataset citys --input-pic $img --out-dir /home/hufq/eval
