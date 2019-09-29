ef16aac7319e61a465ab27917080fa834d21c8a2
CodePlay2016 <13269308268@163.com>
2019-09-29 17:03:08 +0800

    config train

--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -1,1 +1,2 @@
-CUDA_VISIBLE_DEVICES=5 nohup python train.py --model deeplabv3 --backbone resnet152 --dataset citys --lr 0.0001 --epochs 200 --workers 0 --save-dir ../model > output3.log
+#CUDA_VISIBLE_DEVICES=5 nohup python train.py --model deeplabv3 --backbone resnet101 --dataset mapillary --lr 0.0001 --epochs 50 --workers 0 --save-dir ../model > output3.log
+CUDA_VISIBLE_DEVICES=5 python train.py --model deeplabv3 --backbone resnet101 --dataset mapillary --lr 0.0001 --epochs 50 --workers 0 --save-dir ../model
