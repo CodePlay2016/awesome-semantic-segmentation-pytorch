a292d5889059d82139c9f616745048180ac90616
CodePlay2016 <13269308268@163.com>
2019-09-30 11:11:12 +0800

    can start training mapillary with full original label

--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -1,2 +1,2 @@
-#CUDA_VISIBLE_DEVICES=5 nohup python train.py --model deeplabv3 --backbone resnet101 --dataset mapillary --lr 0.0001 --epochs 50 --workers 0 --save-dir ../model > output3.log
-CUDA_VISIBLE_DEVICES=5 python train.py --model deeplabv3 --backbone resnet101 --dataset mapillary --lr 0.0001 --epochs 50 --workers 0 --save-dir ../model
+CUDA_VISIBLE_DEVICES=5 nohup python train.py --model deeplabv3 --backbone resnet101 --dataset mapillary --lr 0.0001 --epochs 50 --workers 0 --save-dir ../model > output_mapi_full.log
+#CUDA_VISIBLE_DEVICES=5 python train.py --model deeplabv3 --backbone resnet101 --dataset mapillary --lr 0.0001 --epochs 50 --workers 0 --save-dir ../model
