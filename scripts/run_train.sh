891ce6d0bcc8bf14d440bb7fb662f6a8af29df91
CodePlay2016 <13269308268@163.com>
2019-09-30 13:43:51 +0800

    config label mapping and num_epoch

--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=4 nohup python train.py --model deeplabv3 --backbone resnet101 --batch-size 8 --dataset mapillary --lr 0.0001 --epochs 50 --workers 0 --save-dir ../model > output_mapi_selected.log
+CUDA_VISIBLE_DEVICES=4 nohup python train.py --model deeplabv3 --backbone resnet101 --batch-size 8 --dataset mapillary --lr 0.0001 --epochs 100 --workers 0 --save-dir ../model > output_mapi_selected.log
 #CUDA_VISIBLE_DEVICES=5 python train.py --model deeplabv3 --backbone resnet101 --dataset mapillary --lr 0.0001 --epochs 50 --workers 0 --save-dir ../model
