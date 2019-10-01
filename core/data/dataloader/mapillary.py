472591a205b86fb3aa0ad4fdf3af7ccf2f54efbd
CodePlay2016 <13269308268@163.com>
2019-10-01 22:37:59 +0800

    config full label training

--- core/data/dataloader/mapillary.py
+++ core/data/dataloader/mapillary.py
@@ -44,15 +44,17 @@ class MapillarySegmentation(SegmentationDataset):
         assert (len(self.images) == len(self.mask_paths))
         if len(self.images) == 0:
             raise RuntimeError("Found 0 images in subfolders of:" + root + "\n")
-        self.valid_classes = self.VALID_CLASS
-        # self._key = np.array([-1, -1, 1, 2, 2, 2, -1, 0, 0, 1,
-        #                       0, 6, 0, 0, 0, 6, -1, -1, -1, 5,
-        #                       7, 7, 7, 0, 0, -1, -1, -1, -1, 6,
-        #                       -1,-1, -1, -1, 8, -1, 0, -1, 8, -1,
-        #                       -1, 0, 8, 0, -1, 8, -1, 8, -1, -1,
-        #                       -1, 8, 3, 4, 4, 4, 4, 3, 4, 4,
-        #                       4, 4, 4, -1, -1, -1])
-        self._key = np.arange(0, self.NUM_CLASS)
+        self.valid_classes = [ 2,  3,  4,  5,  7,  8,  9, 10, 11, 12, 13, 14, 15, 19, 20, 21, 22,
+                              23, 24, 29, 34, 36, 38, 41, 42, 43, 45, 47, 51, 52, 53, 54, 55, 56,
+                              57, 58, 59, 60, 61, 62]
+        self._key = np.array([-1, -1, 1, 2, 2, 2, -1, 0, 0, 1,
+                              0, 6, 0, 0, 0, 6, -1, -1, -1, 5,
+                              7, 7, 7, 0, 0, -1, -1, -1, -1, 6,
+                              -1,-1, -1, -1, 8, -1, 0, -1, 8, -1,
+                              -1, 0, 8, 0, -1, 8, -1, 8, -1, -1,
+                              -1, 8, 3, 4, 4, 4, 4, 3, 4, 4,
+                              4, 4, 4, -1, -1, -1])
+        # self._key = np.arange(0, self.NUM_CLASS)
         self._mapping = np.array(range(0, len(self._key))).astype('int32')
 
     def _class_to_index(self, mask):
