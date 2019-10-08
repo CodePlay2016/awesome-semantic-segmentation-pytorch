64af3b70a36a3cf2a9f68838a46eeab1cd5626ec
CodePlay2016 <13269308268@163.com>
2019-10-08 13:18:14 +0800

    show demo/ debug

--- core/utils/visualize.py
+++ core/utils/visualize.py
@@ -91,6 +91,7 @@ def putpalette(npimg, pallete, dataset='pascal_voc'):
     out_b = np.zeros((npimg.shape[0], npimg.shape[1]))
     i_flag = True if dataset == 'mapillary' else False
     num_class = 10 if i_flag else 19
+    hist = np.histogram(npimg)
     count = dict({})
     for i in range(num_class):
         k = datasets[dataset].KEY[i]+1 if i_flag else i
@@ -100,6 +101,7 @@ def putpalette(npimg, pallete, dataset='pascal_voc'):
         out_g = np.where(index, np.ones_like(out_r) * pallete[3*k+1], out_g)
         out_b = np.where(index, np.ones_like(out_r) * pallete[3*k+2], out_b)
     print('count:  ', count)
+    print('hist:  ', hist)
     img = np.stack([out_r, out_g, out_b], axis=-1)
     return img
 
