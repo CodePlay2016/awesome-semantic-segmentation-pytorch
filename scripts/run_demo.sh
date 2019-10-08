7609158de4c1827ebf781014e6bcd7cd9f8852a7
CodePlay2016 <13269308268@163.com>
2019-10-08 14:34:54 +0800

    show demo/ debug

--- scripts/run_demo.sh
+++ scripts/run_demo.sh
@@ -1,5 +1,5 @@
 # python demo.py --model deeplabv3_resnet101_citys --dataset citys --input-pic /home/hufq/data/leftImg8bit_trainvaltest/leftImg8bit/test/berlin/berlin_000192_000019_leftImg8bit.png --out-dir /home/hufq/eval
-CUDA_VISIBLE_DEVICE=2 python demo.py --model deeplabv3_resnet101_mapillary --save-folder ../model --dataset mapillary --input-pic /home/hufq/data/apollo_selected/171206_060208296_Camera_5.png --out-dir /home/hufq/eval --repetition 1
+CUDA_VISIBLE_DEVICE=2 python demo.py --model deeplabv3_resnet101_mapillary --save-folder ../model --dataset mapillary --input-pic /home/hufq/data/apollo_selected/171206_060208296_Camera_5.jpg --out-dir /home/hufq/eval --repetition 1
 # for img in '/home/hufq/data/apollo_selected'/*5.jpg
 # do
 #     python demo.py --model deeplabv3_resnet101_citys --dataset citys --input-pic $img --out-dir /home/hufq/eval
