769eaa5a1849304a3c265b1dda3af7986f6a5068
CodePlay2016 <13269308268@163.com>
2019-10-28 15:27:13 +0800

    demo changyang | debug

--- scripts/run_demo.sh
+++ scripts/run_demo.sh
@@ -1,6 +1,6 @@
 # python demo.py --model deeplabv3_resnet101_citys --dataset citys --input-pic /home/hufq/data/leftImg8bit_trainvaltest/leftImg8bit/test/berlin/berlin_000192_000019_leftImg8bit.png --out-dir /home/hufq/eval
 CUDA_VISIBLE_DEVICES=6 python demo.py \
-    --model deeplabv3_resnet101_mapillary \
+    --model deeplabv3_resnet152_mapillary \
     --save-folder ../model/mapillary \
     --dataset mapillary \
     --input-pic /home/hufq/test_changyang.png \
